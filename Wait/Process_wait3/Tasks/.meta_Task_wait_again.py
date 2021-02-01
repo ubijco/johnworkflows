34a70840a4eec98337856f54b4046e69bbb9c3c9
ubijco <>
2021-02-01 17:16:20 +0000

    Updating BPMN on  - 01-02-2021 17:16:20 - ncroot

--- Wait/Process_wait3/Tasks/.meta_Task_wait_again.py
+++ Wait/Process_wait3/Tasks/.meta_Task_wait_again.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1612199763131</value>
+            <value>1612199780853</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1612199763126</value>
+            <value>1612199780850</value>
         </entry>
         <entry>
             <key>TAG</key>
